d2e18a25aeae40d50115522aea633848b2e4ae78
S1NDIK4T <Sindikatas01@gmail.com>
2024-05-21 23:05:48 +0400

    Ввод параметров генерации, вывод информации о текущих параметрах

--- PassGen.py
+++ PassGen.py
@@ -5,7 +5,6 @@ class Password_Generation:
         self.__Passwords_amount = 1
         self.get_param_info()
 
-
     def change_amount_simbols(self):
         new_amnt = int(input("Введите необходимое количество символов в пароле >>> "))
         if isinstance(new_amnt, int):
@@ -13,9 +12,10 @@ class Password_Generation:
         else:
             print("Введите целое число")
             self.change_amount_simbols()
+
     def change_generation_type(self):
         new_gen_type = int(input('Выберите тип генерации \n1 - Буквы верхнего и нижнего регистра\n2 - Буквы и цифры\n'
-            '3 - Буквы, цифры и спец.символы\n'))
+                                 '3 - Буквы, цифры и спец.символы\n'))
         if isinstance(new_gen_type, int):
             self.__Generation_type = new_gen_type
         else:
@@ -29,17 +29,18 @@ class Password_Generation:
         else:
             print("Введите целое число")
             self.change_passwords_amount()
+
     def change_parameters(self):
         self.change_amount_simbols()
         self.change_generation_type()
         self.change_passwords_amount()
-
 
     def get_param_info(self):
         print(f'Текущие параметры генерации:\n'
               f'Количество символов - {self.__Amount_Simbols}\n'
               f'Генерируется паролей - {self.__Passwords_amount}\n'
               f'Тип генерации - {self.get_gen_type()}')
+
     def get_gen_type(self):
         if self.__Generation_type == 1:
             return 'Буквы верхнего и нижнего регистра'
